6227afae255011722b7d41d98bf11331a62a07d5
EricFerguson76 <turtle0888@gmail.com>
2019-11-06 14:52:30 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
       </p>
     </div>
   );
