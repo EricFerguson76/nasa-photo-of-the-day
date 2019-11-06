0cb4d42dff1201c6593494cfefe22e0fdaf7dd0a
EricFerguson76 <turtle0888@gmail.com>
2019-11-06 15:24:50 -0600

    Added to Nasa.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import Nasa from './components/Nasa'
 
 function App() {
   return (
     <div className="App">
-      <p>
-      </p>
+      <Nasa />
     </div>
   );
 }
--- src/components/Nasa.js
+++ src/components/Nasa.js
@@ -1,2 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+
+
+function Nasa() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=8xCFCxz63Z69ocezgbo6DW81cPZziY3VLPjuVuFP')
+      .then(response => {
+        console.log(response.data)
+        setData(response.data)
+      })
+      .catch(error => {
+        console.log('data not returned', error)
+      });
+
+  }, [])
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Nasa;
